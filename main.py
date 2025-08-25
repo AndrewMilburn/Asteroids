f7a84c8d4ac82f4e399ee9e51e707c21dd9969cd
Andrew Milburn <sidbell.069@gmail.com>
2025-08-25 15:36:54 +0100

    Going into Ch 2 Lesson 5 - Draw Player

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,10 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        print(f"FPS:{1/dt}")
+
+
 
 
 if __name__ == "__main__":
