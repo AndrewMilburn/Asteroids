fca2ef8f475fd4f5dd9f25306a59dc440a861ad9
Andrew Milburn <sidbell.069@gmail.com>
2025-08-27 09:12:34 +0100

    After Chaper 2 - Player is moving

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
 
     while True:
@@ -15,12 +19,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+
+
+        updatable.update(dt)
+# drawable.draw(screen) - Can't do this as there is no associated Image for the SPRITE
+# which is what the underlying class is derived from 
+        for stuff in drawable:
+            stuff.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        print(f"FPS:{1/dt}")
-
-
-
+    #    print(f"FPS:{1/dt}")
 
 if __name__ == "__main__":
     main()
