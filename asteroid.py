06605c02be9c8f8e029f7a9d6854f1ac5e679fe3
Andrew Milburn <sidbell.069@gmail.com>
2025-08-29 08:38:36 +0100

    3.01 - Asteroid Generation - Done

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, xpos, ypos, radius):
+        super().__init__(xpos, ypos, radius)
+
+    def draw(self, screen):
+       pygame.draw.circle(screen, pygame.Color('white'), self.position, self.radius, 2) 
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
