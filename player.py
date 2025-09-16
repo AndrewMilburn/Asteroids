fcb2df25fb8ab35c021ad39809d2a91767202ac7
Andrew Milburn <sidbell.069@gmail.com>
2025-09-16 14:48:57 +0100

    3.03 halfway

--- player.py
+++ player.py
@@ -24,18 +24,20 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
+        if keys[pygame.K_o]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_i]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_RETURN]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_a]:
             self.move(-dt)
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_VELOCITY * dt
+
+
 
 
 
